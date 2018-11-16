0345d615813e688d8206f74503ae75ed22519408
xfde <alex.constantin432@gmail.com>
2018-11-16 15:00:00 +0200

    Main no change.

--- app/src/main/java/com/alexc/databasetest/MainActivity.java
+++ app/src/main/java/com/alexc/databasetest/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
